6ad926453c0ce11b957991d6149945d2ac9c5f1d
Yana <yananik3@gmail.com>
2019-05-02 22:37:03 +0300

    work with tabs

---
+++ yoga/js/script.js
@@ -0,0 +1,41 @@
+//window.addEventListener('load');
+window.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+
+  function workWithTab(classNameParentTab, classNameTab, classNameTabContent) {
+    let info = document.querySelector(classNameParentTab),
+        tab = document.querySelectorAll(classNameTab),
+        tabContent = document.querySelectorAll(classNameTabContent);
+
+    function hideTabContent(a) {
+      for (let i = a; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+      }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+      if (tabContent[b].classList.contains('hide')) {
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+      }
+    }
+
+    info.addEventListener('click', function(event) {
+      let target = event.target;
+      if (target && target.classList.contains('info-header-tab')) {
+        for (let i = 0; i < tab.length; i++) {
+          if (target == tab[i]) {
+            hideTabContent(0);
+            showTabContent(i);
+            break;
+          }
+        }
+      }
+    });
+  }
+  
+  workWithTab('.info-header', '.info-header-tab', '.info-tabcontent');
+});
